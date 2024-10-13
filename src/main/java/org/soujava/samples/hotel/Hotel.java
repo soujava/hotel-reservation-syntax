ae7ad79357e3515810b0d2bb931256aba141c780
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-13 07:23:38 +0100

    feat: create hotel resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/samples/hotel/Hotel.java
+++ src/main/java/org/soujava/samples/hotel/Hotel.java
@@ -1,22 +1,27 @@
 package org.soujava.samples.hotel;
 
+import jakarta.data.page.Page;
+import jakarta.data.page.PageRequest;
 import jakarta.data.repository.By;
 import jakarta.data.repository.Delete;
 import jakarta.data.repository.Find;
 import jakarta.data.repository.Repository;
 import jakarta.data.repository.Save;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface Hotel {
 
     @Save
-    Guest checkIn(Guest guest);
+    Room checkIn(Room room);
 
     @Delete
-    void checkOut(Guest guest);
+    void checkOut(Room room);
 
     @Find
-    Optional<Guest> reservation(@By(org.soujava.samples.hotel._Room.NUMBER) int number);
+    Optional<Room> reservation(@By(org.soujava.samples.hotel._Room.NUMBER) int number);
+
+    Page<Room> findBy(PageRequest pageRequest);
 }
