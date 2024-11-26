acb5532907231df5b813f14fd1506a998f6b84a2
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-26 14:49:08 +0000

    feat: include guest grouping group
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/samples/hotel/Guest.java
+++ src/main/java/org/soujava/samples/hotel/Guest.java
@@ -4,6 +4,6 @@ import jakarta.nosql.Column;
 import jakarta.nosql.Embeddable;
 import jakarta.nosql.Entity;
 
-@Entity
+@Embeddable(Embeddable.EmbeddableType.GROUPING)
 public record Guest (@Column String documentNumber, @Column String name) {
 }
